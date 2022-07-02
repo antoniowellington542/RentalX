ee73d229c39a1ff4fa6fcf06f9f1a547fbc14386
antoniowellington542 <antoniowellington542@gmail.com>
2022-07-01 21:00:19 -0300

    Service of Create Category Created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
+// Criar categoria
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
+// Listar categorias
 categoriesRoutes.get('/', (request, response) => {
   const allCategories = categoriesRepository.list();
 
